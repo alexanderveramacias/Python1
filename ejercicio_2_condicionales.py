f77a5a4897ba07e9c2b845a95fe15f654f91518e
XanderVerMac <vera.macias26.av@gmail.com>
2023-03-06 12:09:58 -0500

    se realizaron ejercicios de condicionales

---
+++ ejercicio_2_condicionales.py
@@ -0,0 +1,7 @@
+Nombre=input("ingrese su nombre: ")
+Edad=int(input("ingresa tu edad: "))
+if "ingresa tu nombre" and Edad>18:
+    print("eres mayor de 18 años")
+else:
+    print("no eres mayor de 18 años")
+
