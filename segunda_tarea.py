925a832a88cfa0139279b8cba13862f4665e30d5
XanderVerMac <vera.macias26.av@gmail.com>
2023-03-25 11:22:44 -0500

    realizamos un código con for

---
+++ segunda_tarea.py
@@ -0,0 +1,8 @@
+"""TAREA
+PEDIR AL USUARIO INTRODUCIR EL TEXTO, LUEGO INICIALIZAR EL CONTADOR DE LA PALABRA A1 Y LUEGO RECORRER EL TEXTO POR CARACTER UTILIZANDO UN BUCLE FOR E IMPRIMIR EL RESULTADO
+"""
+
+texto_usuario=input("introducir un texto: ")
+numero_variable=int(input("ingresar un número: "))
+for cadena_repetir in range(0,numero_variable):
+    print(texto_usuario)
